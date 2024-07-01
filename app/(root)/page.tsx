c98ae164fe0dbad8a2df7307f4de341f68866ae0
wildan <21523222@students.uii.ac.id>
2024-07-01 13:55:45 +0700

    Implement the complete layout of Homepage vers 1 and Sidebar

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,8 +1,36 @@
+import HeaderBox from "@/components/HeaderBox";
+import RightSidebar from "@/components/RightSidebar";
+import TotalBalanceBox from "@/components/TotalBalanceBox";
 import React from "react";
 
 const Home = () => {
+    const loggedIn = { firstName: 'Wildan', lastName: 'Ahmad', email: 'makanbang@gmail.com' };
+
     return (
-        <div>Home</div>
+        <section className="home">
+            <div className="home-content">
+                <header className="home-header">
+                    <HeaderBox
+                        type="greeting"
+                        title="Welcome"
+                        user={loggedIn?.firstName || 'Guest'}
+                        subtext="Access and manage your account and transanctions efficiently"
+                    />
+
+                    <TotalBalanceBox
+                        accounts={[]}
+                        totalBanks={1}
+                        totalCurrentBalance={1250.35}
+                    />
+                </header>
+                RECENT TRANSACTION
+            </div>
+            <RightSidebar
+                user={loggedIn}
+                transactions={[]}
+                banks={[{ currentBalance: 123.50 }, { currentBalance: 500.50 }]}
+            />
+        </section>
     )
 }
 
